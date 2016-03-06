0160ec76d72abbc20225422a77741d7e66b44197
Alex Larson <alarson85@gmail.com>
2016-03-06 14:46:28 -0600

    made a few minor changes and completed useable make page

--- makeWish.js
+++ makeWish.js
@@ -4,8 +4,8 @@ angular.module('bedazzled').directive('makeWish', function(){
 
 		template: `
 			<div class="wish-input-wrapper">
-				<input class="wish-input" ng-model="wish" placeholder="I wish I had...">
-				<button class="wish-input-button" ng-click="addWish(wish)">Make It</button>
+				<input class="wish-input" ng-model="wish" placeholder="I wish for ... a sandwich">
+				<p class="wish-input-button" ng-click="addWish(wish)">Make It</p>
 			</div>`
 		, controller: function($scope, firebaseUrl, $firebaseArray){
 			var wishesRef = new Firebase(firebaseUrl.ref + 'wishes');
